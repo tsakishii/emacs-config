ae8adab9763745c9db2c17794803555e655093cd
kiwib0y <caki6ko@yahoo.com>
2024-12-02 09:38:10 +0100

    Add eshell syntax hl
      Experimenting with this configuration. Saw it on github.

--- modules/kw-eshell.el
+++ modules/kw-eshell.el
@@ -5,4 +5,10 @@
         (concat "[" (getenv "USER") "@" (system-name) " "
                 (eshell/pwd) "] " (if (= (user-uid) 0) "# " "λ "))))
 
+(use-package eshell-syntax-highlighting
+  :after eshell
+  :demand t
+  :config
+  (eshell-syntax-highlighting-global-mode +1))
+
 (provide 'kw-eshell)
