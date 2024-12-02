15dabbdfce46a3d4ffef795f92ebe3aae1217e64
kiwib0y <caki6ko@yahoo.com>
2024-12-02 09:30:19 +0100

    Add package module
      This module should be required before every other module. It is the
      one that uses all package sources.

---
+++ modules/kw-package.el
@@ -0,0 +1,22 @@
+(require 'package)
+
+(setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+
+(setq package-install-upgrade-built-in t)
+
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+(provide 'kw-package)
