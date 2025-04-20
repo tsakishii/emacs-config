1e0e7b26f4ca61b60d3859b0435744cf9120b4b7
tsakishii <ts.chris@protonmail.com>
2025-04-20 10:49:28 +0200

    remove: Solarized and Zenburn themes
      Not using those anymore. Modus themes rullz!

--- modules/kw-theme.el
+++ modules/kw-theme.el
@@ -6,15 +6,4 @@
   (setq modus-themes-scale-headings t)
   (load-theme 'modus-vivendi-tinted))
 
-(use-package zenburn-theme
-  :ensure t
-  :config
-  (setq zenburn-scale-org-headlines t))
-
-(use-package solarized-theme
-  :ensure t
-  :config
-  (setq x-underline-at-descent-line t)
-  (setq solarized-high-contrast-mode-line t))
-
 (provide 'kw-theme)
